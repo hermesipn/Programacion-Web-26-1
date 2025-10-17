3cec1f11bf31936b33dea443b8d192de3c5ce071
Hermes Francisco Montes Casiano <hermes.montes@gmail.com>
2025-10-17 08:32:07 -0600

    feat: se versiona el ejemplo de la calculadora
    feat: se suben los ejemplos de ccs, css grid y css flexbox

---
+++ ejemplos/scripts/index.js
@@ -0,0 +1,25 @@
+console.log('Hola mundo del javascript');
+var a;
+console.log('El valor de a es ' + a); // El valor de a es undefined
+
+console.log('El valor de b es ' + b); // El valor de b es undefined
+var w = 'Hola ' + b;
+console.log('El valor de w es '+ w);
+var b;
+
+//console.log('El valor de c es ' + c); // Error de referencia no detectado: c no está definida
+
+let x;
+console.log('El valor de x es ' + x); // El valor de x es undefined
+
+//console.log('El valor de y es ' + y); // Error de referencia no detectada: y no está definida
+//let y;
+
+let vara = 3;
+let varb = "3";
+
+if(vara === varb) {
+    console.log("Son igualeeeees");
+} else {
+    console.log("Son diferentes");
+}
